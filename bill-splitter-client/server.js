78fdb0433b36451be71baf1fdda0beddd01da14f
apoorvdodiya <apoorvdodiya@gmail.com>
2022-11-20 14:12:21 +0530

    added server file in client

---
+++ bill-splitter-client/server.js
@@ -0,0 +1,34 @@
+console.log('inside server.js');
+
+// server.js
+const express = require('express');
+const app = express();
+const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/bill-splitter-client'));
+
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// Instruct the app
+// to use the forceSSL
+// middleware
+// app.use(forceSSL());
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/bill-splitter-client'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8081, () => {
+  console.log('server started on port ', process.env.PORT || 8081);
+});
