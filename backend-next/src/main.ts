57295fc9c9beb3e928145daa2bf841c7f0729610
apoorvdodiya <apoorvdodiya@gmail.com>
2022-12-03 17:54:54 +0530

    backend env changes

--- backend-next/src/main.ts
+++ backend-next/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { env } from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
@@ -11,6 +12,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
